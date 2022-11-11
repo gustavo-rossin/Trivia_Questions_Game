859c7fc9b3244533341677a17a7c785a370b4b47
Rodrigo <rodrigomartins.agro@gmail.com>
2022-11-11 16:07:59 -0300

    Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div className="App-header">
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default Ranking;
