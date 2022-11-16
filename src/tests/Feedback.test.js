a2de9c4d1bb0d286b99a05c0b02c9357ac9a43df
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-11-16 14:38:02 -0300

    initializing req 22

---
+++ src/tests/Feedback.test.js
@@ -0,0 +1,54 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import App from '../App';
+import { act } from 'react-dom/test-utils';
+import data from './helpers/data';
+
+describe('testa página de feedback', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({json: jest.fn().mockResolvedValue(data)});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('testa botão play again', async () => {
+    const {history} = renderWithRouterAndRedux(<App />);
+    const nameInput = screen.getByTestId('input-player-name');
+    const emailInput = screen.getByTestId('input-gravatar-email');
+    const playBtn = screen.getByTestId('btn-play');
+    await act(() => {
+      userEvent.type(nameInput, 'Raphael Righetti');
+      userEvent.type(emailInput, 'rapha@trybe.com');
+      userEvent.click(playBtn);
+    });
+    for (let i = 0; i < 5; i += 1) {
+      userEvent.click(screen.getByTestId('correct-answer'));
+      userEvent.click(screen.getByTestId('btn-next'));
+    }
+    expect(history.location.pathname).toBe('/feedback');
+    const playAgainBtn = screen.getByRole('button', { name: /play again/i });
+    userEvent.click(playAgainBtn);
+    expect(history.location.pathname).toBe('/');
+  });
+  it('testa botão ranking', async () => {
+    const {history} = renderWithRouterAndRedux(<App />);
+    const nameInput = screen.getByTestId('input-player-name');
+    const emailInput = screen.getByTestId('input-gravatar-email');
+    const playBtn = screen.getByTestId('btn-play');
+    await act(() => {
+      userEvent.type(nameInput, 'Raphael Righetti');
+      userEvent.type(emailInput, 'rapha@trybe.com');
+      userEvent.click(playBtn);
+    });
+    for (let i = 0; i < 5; i += 1) {
+      userEvent.click(screen.getByTestId('correct-answer'));
+      userEvent.click(screen.getByTestId('btn-next'));
+    }
+    const rankingBtn = screen.getByRole('button', { name: /ranking/i });
+    userEvent.click(rankingBtn);
+    expect(history.location.pathname).toBe('/ranking');
+  });
+});
---
+++ src/tests/helpers/data.js
@@ -0,0 +1,36 @@
+const data = {
+  results: [
+    {
+      question: 'enunciado1',
+      category: 'categoria1',
+      incorrect_answers: ['false'],
+      correct_answer: 'true',
+    },
+    {
+      question: 'enunciado2',
+      category: 'categoria2',
+      incorrect_answers: ['false'],
+      correct_answer: 'true',
+    },
+    {
+      question: 'enunciado3',
+      category: 'categoria3',
+      incorrect_answers: ['false'],
+      correct_answer: 'true',
+    },
+    {
+      question: 'enunciado4',
+      category: 'categoria4',
+      incorrect_answers: ['false'],
+      correct_answer: 'true',
+    },
+    {
+      question: 'enunciado5',
+      category: 'categoria5',
+      incorrect_answers: ['false'],
+      correct_answer: 'true',
+    },
+  ],
+};
+
+export default data;
